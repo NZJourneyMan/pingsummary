f8e367f7a64add9ec27e6013d69f90c74ddb9acc
Mark Wilson <mark-git@omzig.co.uk>
2021-04-20 13:02:30 +1200

    Set graph file to auto reload

--- static/scripts/ping_summary.js
+++ static/scripts/ping_summary.js
@@ -3,17 +3,24 @@ var app = new Vue({
     data() {
         return {
             pickedDate: "",
-            imageFile: ""
+            imageFile: "",
+            myTimer: null        
         };
     },
     methods: {
         submit() {
             if (this.pickedDate) {
+                if (this.myTimer) { 
+                    clearInterval(this.myTimer);
+                }
+                this.myTimer = setTimeout(this.submit, 300000);
                 this.imageFile = '/static/images/' + this.pickedDate + ".png?dummy=" + Math.random();
             }
         },
         brokenImage() {
             this.imageFile = "";
+            clearInterval(this.myTimer)
+            this.myTimer = null;
         },
     }
 })
