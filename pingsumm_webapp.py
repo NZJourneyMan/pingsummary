f9234f7bd411698c1677ab9057bbd5fad849c441
Mark Wilson <mark-git@omzig.co.uk>
2021-04-20 13:06:42 +1200

    Change port number

--- pingsumm_webapp.py
+++ pingsumm_webapp.py
@@ -32,4 +32,4 @@ def do500(e):
 
 if __name__ == '__main__':
     # app.debug = True
-    app.run()
+    app.run(host='0.0.0.0', port=2233)
--- startapp.sh
+++ startapp.sh
@@ -6,7 +6,7 @@ gunicorn \
     --reload \
     --reload-extra-file $ROOTDIR/static/scripts/ping_summary.js \
     --reload-extra-file $ROOTDIR/templates/ping_summary.html \
-    --bind 0.0.0.0:5000 \
+    --bind 0.0.0.0:2233 \
     --access-logfile - \
     --log-file - \
     pingsumm_webapp:app
